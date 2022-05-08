74807fbbf9adb157c92a0c556636f8c8579e3c6b
Rafael Niebles <rnieblesealo@gmail.com>
2022-05-08 16:29:22 -0400

    Updated scene

---
+++ Assets/UIHandler.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIHandler : MonoBehaviour{
+    //note, this is to function as a temporary way to handle wordle in ui.
+    //must do some reworking if more ui elems are to be added.
+
+    public GameObject wordlePanel;
+    
+    private void Start(){
+        
+    }
+
+    private void Update(){
+        
+    }
+}
